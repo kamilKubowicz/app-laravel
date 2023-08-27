d7828aaab6afafcb10be12d360b12f3e63bfe946
K.Kubowicz <k.kubowicz@global4net.com>
2023-08-27 10:28:15 +0200

    added relation with post for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -53,6 +54,12 @@ class User extends Authenticatable
     public function hasEditorPermission(): bool
     {
         $role = $this->role;
+
         return  ($role === self::ROLE_ADMIN || $role === self::ROLE_EDITOR);
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'author_id');
     }
 }
