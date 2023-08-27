4260d3e7e80e755941dff2b3233709dd04899fb0
K.Kubowicz <k.kubowicz@global4net.com>
2023-08-27 10:26:41 +0200

    added crud for post

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,11 +20,17 @@ use App\Http\Controllers\Api\LoginController;
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
-Route::post('/users/edit', [UserController::class, 'edit'])->middleware('auth:sanctum');
-Route::post('/users/remove', [UserController::class, 'remove'])->middleware('auth:sanctum');
-Route::post('/users/new', [UserController::class, 'nwq'])->middleware('auth:sanctum');
 
+Route::post('/users/edit/{id}', [UserController::class, 'edit'])->middleware('auth:sanctum');
+Route::post('/users/remove/{id}', [UserController::class, 'remove'])->middleware('auth:sanctum');
+Route::post('/users/new', [UserController::class, 'new'])->middleware('auth:sanctum');
 Route::get('/users/index', [UserController::class, 'index'])->middleware('auth:sanctum');
+
+Route::post('/posts/new', [PostController::class, 'new'])->middleware('auth:sanctum');
+Route::post('/posts/remove/{id}', [PostController::class, 'remove'])->middleware('auth:sanctum');
+Route::post('/posts/edit/{id}', [PostController::class, 'edit'])->middleware('auth:sanctum');
+Route::get('/posts/index', [PostController::class, 'index']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
